69f2cece03bf65f598f324b0600076dbd5f7669c
harlen lopez <harlen.d.lopez@gmail.com>
2019-12-03 16:15:54 -0800

    changes made to about-me

--- js/app.js
+++ js/app.js
@@ -1,40 +1,36 @@
 'use strict';
 
 var userName = prompt('Hello, may i please have your name?');
-alert(userName);
 var hello = userName + ' you are invited to play a game to get to know me a little bit better!';
 alert(hello);
-var myName = prompt( userName + ', what is my name');
-alert(myName);
-var hometown = prompt(userName + ', where am I originally from?');
-alert(hometown);
-var activity = prompt(userName + ', what is my favorite activity');
-alert(activity);
-alert('Thank you' + userName + ' for taking part of this short questionaire, further on you will learn more about me');
 
-// console.log('javascript is UP');
+console.log('javascript is UP');
 
-var userName = prompt('Hello, may i please have your name?');
 console.log('user stated their name was ' + userName);
 
-var lowerCaseUsername = userName.toLowerCase();
-if(lowerCaseUsername === 'harlen') {
-  alert('hello overlord ' + userName);
-} else {
-  alert('welcome, please enjoy my site.');
-}
+var myName = prompt( userName + ', what is my name');
 if (myName === 'harlen'){
     alert('great job' + userName + '!');
 } else {
     alert('sorry ' + userName + ', that is incorrect.');
 }
 
+var lowerCaseUsername = myName.toLowerCase();
+if(lowerCaseUsername === 'harlen') {
+  alert('hello overlord ' + myName);
+} else {
+  alert('welcome, please enjoy my site.');
+}
+
+var hometown = prompt(userName + ', where am I originally from?');
 if(hometown === 'placerville'){
     alert('wow' + userName + ', how did you know that?');
 } else {
     alert( ' oh, too bad' + userName + '.');
 }
-
+var activity = prompt(userName + ', what is my favorite activity');
 if(activity === 'woodworking');{
     alert('you are on fire, great job!!!');
 }
+
+alert('Thank you' + userName + ' for taking part of this short questionaire, further on you will learn more about me');
